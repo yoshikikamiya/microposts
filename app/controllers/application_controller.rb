93a6ec62a3a1bd63c8323c1e8e2ab1dc2bba31de
yoshiki.kamiya <kamiya0124510@gmail.com>
2019-06-30 13:22:43 +0000

    logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
     
     private
     
-    def require_user_logged_in
-        unless logged_in?
-        redirect_to login_url
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
     end
-end
+  end
 end
