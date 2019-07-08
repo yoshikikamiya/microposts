9fc28d7556eb917769680ae0a489da356d78c3c7
yoshiki.kamiya <kamiya0124510@gmail.com>
2019-07-08 08:11:37 +0000

    timeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     
     include SessionsHelper
     
+    
   private
 
   def require_user_logged_in
@@ -11,5 +12,7 @@ class ApplicationController < ActionController::Base
   end
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
